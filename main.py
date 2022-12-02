d4853a9c375b585f40a4a07c3d793a9b62c5c313
tech-jan <trewej@hotmail.com>
2022-12-02 23:14:08 +0100

    made the output in console more readable

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ def compare(start, chosen):
     print(f'{start["name"]} has {start["follower_count"]} likes')
     print(f'{chosen["name"]} has {chosen["follower_count"]} likes')
     if start["follower_count"] >= chosen["follower_count"]:
+        print(f'{start["name"]} has more than {chosen["name"]}')
         return True
     else:
-        print("--")
+        print(f'{chosen["name"]} has more than {start["name"]}')
         return False
 
 def check_answer(input, start_data, chosen_data):
@@ -30,11 +31,13 @@ def check_answer(input, start_data, chosen_data):
             return True
         else:
             return False
-    if input == "h":
+    elif input == "h":
         if not compare(start_data, chosen_data):
             return True
         else:
             return False
+    else:
+        print("please use a usefull input like h or l")
 
 
 
@@ -48,14 +51,14 @@ def game_run():
         print(f"({vs}+\n")
         chosen_data = data[random.randint(0, len(data) - 1)]
         print(f'compare {chosen_data["name"]} ,a {chosen_data["description"]} ,from {chosen_data["country"]} \n')
-        my_input = input(f'has {chosen_data["name"]} one higher [h] ore lower [l] instalikes?')
+        my_input = input(f'has {chosen_data["name"]} one higher [h] ore lower [l] instalikes?').lower()
 
         ##variant 2 with an function
         if check_answer(my_input,start_data,chosen_data):
-            print(f'nice one {start_data["name"]} won against {chosen_data["name"]} ')
+            print(f'nice one you guessed rioght ')
             score += 1
         elif not check_answer(my_input, start_data, chosen_data):
-            print(f'you lose {start_data["name"]} loses against {chosen_data["name"]}')
+            print(f'you lose wrong guess')
             runner = False
 
         start_data = chosen_data
